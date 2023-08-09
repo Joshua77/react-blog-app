3f35427cae79b3d74a9c91a83471f6f41439b73a
Joshua Akinola <46546654+Joshua77@users.noreply.github.com>
2023-08-09 14:32:31 +0100

    Update RegisterPage.js

--- src/RegisterPage.js
+++ src/RegisterPage.js
@@ -6,7 +6,7 @@ export default function RegisterPage() {
   const [password, setPassword] = useState('');
   async function register(ev) {
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/register', {
+    const response = await fetch('https://react-blog-app-api.vercel.app/register', {
       method: 'POST',
       body: JSON.stringify({username,password}),
       headers: {'Content-Type':'application/json'},
